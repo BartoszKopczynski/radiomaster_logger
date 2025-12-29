ff4009014de297a031ec7622a62afdab0e042b40
Bartosz89k <bartosz89.k@gmail.com>
2025-12-29 12:34:39 +0100

    add missing file updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,141 @@
-#include <iostream>
-#include <thread>
-#include <atomic>
-#include <chrono>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include "JoystickReader.hpp"
+#include "StickWidget.hpp"
+#include "StickState.hpp"
 
 #include "RingBuffer.hpp"
-#include "JoystickReader.hpp"
 #include "BinaryLogger.hpp"
+
+#include <atomic>
+#include <thread>
+#include <iostream>
+
 
 constexpr size_t BUFFER_SIZE = 40;
 
+inline void applyEventToState(const StickEvent& evt, StickState& state) {
+    const float v = evt.value / 32767.0f;
+
+    if (evt.stick == Stick::Left) {
+        if (evt.axis == AxisType::X) state.lx = v;
+        else                         state.ly = v;
+    } else {
+        if (evt.axis == AxisType::X) state.rx = v;
+        else                         state.ry = v;
+    }
+}
+
+
 int main() {
-    RingBuffer<StickEvent, BUFFER_SIZE> buffer;
+    if (!glfwInit()) {
+        std::cerr << "GLFW init failed\n";
+        return -1;
+    }
+
+    GLFWwindow* window =
+        glfwCreateWindow(800, 400, "FPV Stick Monitor", nullptr, nullptr);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    //REQUIRED when using glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    StickState state;
     std::atomic<bool> running{true};
 
-    JoystickReader joystick("/dev/input/js0");
-    BinaryLogger<BUFFER_SIZE> logger("data/boxer_sticks.bin", buffer, running);
-
+//     JoystickReader reader("/dev/input/js0", state);
+    JoystickReader reader("/dev/input/js0");
+    //std::thread joystickThread(&JoystickReader::run, &reader, std::ref(running));
+    
+    
+    RingBuffer<StickEvent, BUFFER_SIZE> buffer;
+    //std::atomic<bool> running{true};
+    
+    BinaryLogger<BUFFER_SIZE> logger("data/boxer_sticks2.bin", buffer, running);
     std::thread loggerThread(&BinaryLogger<BUFFER_SIZE>::run, &logger);
 
     std::cout << "Logging stick data (Ctrl+C to stop)...\n";
+    
+    //std::thread joystickThread;  // declare outside
+    //try {
+    //    JoystickReader reader("/dev/input/js0", state);
+    //    std::thread joystickThread(&JoystickReader::run, &reader, std::ref(running));
+    //} 
+    //    catch (const std::runtime_error &e) {
+    //    std::cerr << "Joystick not available: " << e.what() << "\n";
+    //}
 
-    while (running) {
 
+
+    while (!glfwWindowShouldClose(window)) {
         StickEvent evt;
-        if (joystick.readEvent(evt)) {
+        if (reader.readEvent(evt)) {
             std::cout << " evt::: " << evt.value << std::endl;
             buffer.push(evt); // drop if full
+            applyEventToState(evt, state);  //GUI update
         }
-        std::this_thread::sleep_for(std::chrono::microseconds(200));
+        
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Sticks");
+        DrawStick2D("Left Stick", state.lx, state.ly);
+        ImGui::SameLine();
+        DrawStick2D("Right Stick", state.rx, state.ry);
+        ImGui::End();
+
+        ImGui::Render();
+
+        int w, h;
+        glfwGetFramebufferSize(window, &w, &h);
+        glViewport(0, 0, w, h);
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
     }
 
+    running = false;
+    //if (joystickThread.joinable()) {
+    //    joystickThread.join();
+    //}
+
+    //joystickThread.join();
+    
     loggerThread.join();
+        
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
-
